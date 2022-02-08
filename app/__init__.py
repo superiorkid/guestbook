47d6d678daff5d0a540d017df4c3d153b976b3e3
superiorkid <bayiPetani@protonmail.com>
2022-02-08 21:13:24 +0800

    add flask-bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from config import Config
 from flask_pymongo import PyMongo
 from faker import Faker
+from flask_bootstrap import Bootstrap4
 
 app = Flask(__name__)
 app.config.from_object(Config)
 mongodb_client = PyMongo(app)
 db = mongodb_client.db
 fake = Faker()
+bootstrap = Bootstrap4(app)
 
 from app import routes
