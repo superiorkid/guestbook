46189ef86a0a49959da9e5e9f7d84ab74db98abb
superiorkid <bayiPetani@protonmail.com>
2022-02-08 08:27:43 +0800

    simple form

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,11 @@
-from wsgiref.validate import validator
-from xmlrpc.client import Boolean
-from flask_wtf import FlaskForm, RecaptchaField
-from wtforms import StringField, SubmitField, TextAreaField, BooleanField
-from wtforms import DataRequired, Email
+from flask_wtf import FlaskForm
+from flask_wtf.recaptcha import RecaptchaField
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email
 
 class GuestForm(FlaskForm):
-  name =StringField('Name', validators=[DataRequired()])
-  email = StringField('Email', validators=[DataRequired(), Email(granular_message=True)])
+  name = StringField('Name', validators=[DataRequired()])
+  email = StringField('Email', validators=[DataRequired(), Email()])
   message = TextAreaField('Messages', validators=[DataRequired()])
-  captcha = RecaptchaField()
+  recaptcha = RecaptchaField('Recapthha')
   submit = SubmitField('Submit')
