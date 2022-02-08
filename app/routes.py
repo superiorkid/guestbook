12f5bd4a48e542f59d533f9500a96782ba19c97e
superiorkid <bayiPetani@protonmail.com>
2022-02-08 08:26:55 +0800

    route for create, read, delte

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,31 @@
 from app import app, db
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+from .forms import GuestForm
+from bson.objectid import ObjectId
+from bson.json_util import dumps
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def guestbook():
-  msg = 'Hello'
-  return render_template('index.html', msg=msg)
+  forms = GuestForm()
+  
+  if forms.validate_on_submit():
+    name = forms.name.data
+    email = forms.email.data
+    message = forms.message.data
+    
+    db.guestbook.insert_one({
+      'name': name,
+      'email': email,
+      'message': message
+    })
+    
+    return redirect(request.url)
+    
+  comment = db.guestbook.find()
+    
+  return render_template('index.html', forms=forms, comment=comment)
+
+@app.route('/delete/<guestId>')
+def delete(guestId):
+  db.guestbook.delete_one({'_id': ObjectId(guestId)})
+  return redirect(url_for('guestbook'))
