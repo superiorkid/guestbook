c9c74b3c342ca444ecfdc9caaaba3eb870a538ad
superiorkid <bayiPetani@protonmail.com>
2022-02-28 21:27:02 +0800

    DESCENDING sorting collections

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,5 @@
-from email.policy import default
-from re import search
-
-from click import get_os_args
 from app import app, db, fake
-from flask import render_template, request, redirect, url_for, jsonify
+from flask import render_template, request, redirect
 from .forms import GuestForm
 from bson.objectid import ObjectId
 from flask_paginate import Pagination, get_page_parameter
@@ -31,20 +27,13 @@ def guestbook():
   forms.email.data = fake.email()
   forms.message.data = fake.text()
   
-  search = False
-  
-  q = request.args.get('q')
-  
-  if q:
-    search = True
-  
   # paginate
   page = request.args.get(get_page_parameter(), type=int, default=1)
-  per_page = 5
+  per_page = 10
   collection = db.guestbook
   total = collection.count_documents({})
-  all_guest = collection.find().skip((page - 1) * per_page).limit(per_page)
-  pagination = Pagination(page=page, per_page=per_page, total=total, search=search, record_name="allguest")
+  all_guest = collection.find().sort('_id', pymongo.DESCENDING).skip((page - 1) * per_page).limit(per_page)
+  pagination = Pagination(page=page, per_page=per_page, total=total, record_name="allguest")
   
   return render_template(
     'index.html',
